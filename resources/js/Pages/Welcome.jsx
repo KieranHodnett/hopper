9b89b7219b3f6e8f852f7e4f9e9b4cc320ef338e
Kieran Hodnett <kieran.hodnett@magnushealth.com>
2025-01-26 16:49:58 -0500

    init

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,8 @@
+export default function Welcome(){
+    return <div>
+        <h1>Welcome to Hopper!</h1>
+        <p>
+            Hopper is a Laravel starter kit that comes with a modern front-end development workflow.
+        </p>
+           </div>
+}
